4fcbedd25f30cf2135960e977d9599cb60160bf5
Simo-Pekka Kerkelä <simopekka1990@gmail.com>
2014-03-10 19:06:48 +0200

    solved problem 3

---
+++ problem3/euler3.py
@@ -0,0 +1,22 @@
+import numbers
+
+def largest_prime(num):
+    largest = 0
+    i = 2
+    while i * i <= num:
+        if num % i == 0:
+            num = num / i
+            largest = i
+        else:
+            i += 1
+
+    if num > largest:
+        largest = num
+    return largest
+
+def main():
+    testnum = 600851475143
+    print largest_prime(testnum)
+
+if __name__ == '__main__':
+    main()
